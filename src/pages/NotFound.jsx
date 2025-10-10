57dead2d7734a876c625598e6f0c8418a36bf5d3
AlaiWebDev <alainwebdev@free.fr>
2025-10-10 11:14:27 +0200

    END tailwind to boostrap

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom';
 function NotFound() {
   return (
-    <div className="p-4">
-      <h1 className="text-2xl m-5 font-bold text-red-600">Oups, la page que vous avez demandé est introuvable…</h1>
-      <Link to="/" className="hover:bg-green-800 inline-block border border-blue-500 rounded m-5 py-1 px-3 bg-blue-500 text-white">Retour accueil</Link>
+    <div className='p-4'>
+      <h1 className='fw-bold mb-4 text-center'>Oups, la page que vous avez demandé est introuvable…</h1>
+      <Link to="/" className='btn btn-primary m-4'>Retour accueil</Link>
     </div>
   )
 }
